168e6581a7eeb8f21f4624d957d824afb291c797
Gareth Tribello <gareth.tribello@gmail.com>
2020-11-03 19:02:45 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+import sympy as sy
+
+# I have defined the symbols for you to use in your expressions
+x, y, k = sy.symbols('x y k')
+# You need to add a sympy command after the comma here to calculate 1/k here
+k = 
+print("The value of k is", k  )  
